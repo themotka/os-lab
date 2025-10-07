5a08ea56084c491cfdfddc210c89c68056091529
Matthew Wideman <74961598+themotka@users.noreply.github.com>
2025-10-07 15:46:10 +0300

    complete project

---
+++ SharedFiles/IEcoLab1.h
@@ -0,0 +1,58 @@
+﻿/*
+ * <кодировка символов>
+ *   Cyrillic (UTF-8 with signature) - Codepage 65001
+ * </кодировка символов>
+ *
+ * <сводка>
+ *   IEcoLab1
+ * </сводка>
+ *
+ * <описание>
+ *   Данный заголовок описывает интерфейс IEcoLab1
+ * </описание>
+ *
+ * <ссылка>
+ *
+ * </ссылка>
+ *
+ * <автор>
+ *   Copyright (c) 2018 Vladimir Bashev. All rights reserved.
+ * </автор>
+ *
+ */
+
+#ifndef __I_ECOLAB1_H__
+#define __I_ECOLAB1_H__
+
+#include "IEcoBase1.h"
+
+/* IEcoLab1 IID = {277FC00C-3562-4096-AFCF-C125B94EEC90} */
+#ifndef __IID_IEcoLab1
+static const UGUID IID_IEcoLab1 = {0x01, 0x10, {0x27, 0x7F, 0xC0, 0x0C, 0x35, 0x62, 0x40, 0x96, 0xAF, 0xCF, 0xC1, 0x25, 0xB9, 0x4E, 0xEC, 0x90}};
+#endif /* __IID_IEcoLab1 */
+
+typedef struct IEcoLab1* IEcoLab1Ptr_t;
+
+typedef struct IEcoLab1VTbl {
+
+    /* IEcoUnknown */
+    int16_t (ECOCALLMETHOD *QueryInterface)(/* in */ IEcoLab1Ptr_t me, /* in */ const UGUID* riid, /* out */ voidptr_t* ppv);
+    uint32_t (ECOCALLMETHOD *AddRef)(/* in */ IEcoLab1Ptr_t me);
+    uint32_t (ECOCALLMETHOD *Release)(/* in */ IEcoLab1Ptr_t me);
+
+    /* IEcoLab1 */
+    int16_t (ECOCALLMETHOD *MyFunction)(/* in */ IEcoLab1Ptr_t me, /* in */ char_t* Name, /* out */ char_t** CopyName);
+
+    /* Вычисление гамма-функции для разных типов */
+    int16_t (ECOCALLMETHOD *gamma_float)(/* in */ IEcoLab1Ptr_t me, /* in */ float x, /* out */ float* result);
+    int16_t (ECOCALLMETHOD *gamma_double)(/* in */ IEcoLab1Ptr_t me, /* in */ double x, /* out */ double* result);
+    int16_t (ECOCALLMETHOD *gamma_longdouble)(/* in */ IEcoLab1Ptr_t me, /* in */ long double x, /* out */ long double* result);
+
+} IEcoLab1VTbl, *IEcoLab1VTblPtr;
+
+interface IEcoLab1 {
+    struct IEcoLab1VTbl *pVTbl;
+} IEcoLab1;
+
+
+#endif /* __I_ECOLAB1_H__ */
